0c635cafd3eb32e7a9a04c87678de87c9c693d4f
Krejcza <155578815+Krejcza@users.noreply.github.com>
2024-02-27 19:46:20 +0100

    Udělány další změny - Přidány odkazy a celá fotogalerie + kalndář

---
+++ script-wea.js
@@ -0,0 +1,15 @@
+const api = 'https://api.openweathermap.org/data/2.5/weather?lat=50.092777&lon=15.807426&appid=0d6ad72d49a7ad53e18459699d5f5bb0'
+
+const weatherIcon = document.querySelector('.weather-icon')
+
+
+
+document.querySelector('temp').innerHTML
+
+
+
+
+
+
+
+ 
