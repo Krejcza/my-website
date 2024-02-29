dca335bb11fc07060845fc5087498fdd03d427c4
Krejcza <155578815+Krejcza@users.noreply.github.com>
2024-02-29 19:07:14 +0100

    přidány souřadnice s mapou, udělána responzivita a hamburger menu aktivní

--- script-scrolling.js
+++ script-scrolling.js
@@ -1,1 +1,24 @@
-document.querySelectorAll
+window.onload = function(){
+   
+const menu_btn = document.querySelector('.hamburger')
+const mobile_menu = document.querySelector('.mobile-nav')
+const mobileNavLinks = document.querySelectorAll('.mobile-nav a');
+
+menu_btn.addEventListener('click', function(){
+   menu_btn.classList.toggle('is-active')
+   mobile_menu.classList.toggle('is-active')
+   })
+
+   mobileNavLinks.forEach(link => {
+      link.addEventListener('click', function() {
+          
+          mobile_menu.classList.remove('is-active');
+      });
+  })
+
+}
+
+
+
+
+
